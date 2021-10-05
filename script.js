a34c078d454563ac655fae814739bdcac7e619da
eledias <elenadias@ymail.com>
2021-10-05 18:06:51 +0200

    secondComm

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ if (localStorage.getItem('libri')) { //se questa cosa esiste
 }
 caricaLibri();
 
-const btn = document.querySelector('#btn');
+
 // var listaLibri = [];
 const cerca = document.querySelector('#invia');
 cerca.addEventListener('click', () => {
@@ -63,20 +63,33 @@ cerca.addEventListener('click', () => {
         console.log(risultati);
     })
 });
-btn.addEventListener('click', () => {
+
+const salva = document.querySelector('#salva');
+salva.addEventListener('click', () => {
+    const indice = document.querySelector('#indice').value;
     const titolo = document.querySelector('#titolo').value;
     const autore = document.querySelector('#autore').value;
     const anno = document.querySelector('#anno').value;
     const genere = document.querySelector('#genere').value;
     const prezzo = document.querySelector('#prezzo').value;
-    //istanza oggetto
-    let libro = new Libro(titolo, autore, anno, genere, prezzo);
-    libro._annoPubb = anno;
-    libro._genere = genere;
-    libro._prezzo = prezzo;
-    let libroScelto = libro.titoloCompletoLibro();
-    console.log(libroScelto);
-    listaLibri.push(libro);
+    //se l'indice è = -1 allora fai il nuovo oggetto libro
+    if (indice == -1) { //all'inizio ho messo === e mi dava errore perchè nel form sono stringhe e non dovevo fare il confronto sul TIPO di dato ma sui valori!
+        //istanza oggetto
+        let libro = new Libro(titolo, autore, anno, genere, prezzo);
+        libro._annoPubb = anno;
+        libro._genere = genere;
+        libro._prezzo = prezzo;
+        let libroScelto = libro.titoloCompletoLibro();
+        console.log(libroScelto);
+        listaLibri.push(libro);
+    } else { //alrimenti : array dei libri[i].titolo  = assegno il value del titolo ecc
+        listaLibri[indice].titolo = titolo;
+        listaLibri[indice].autore = autore;
+        listaLibri[indice]._annoPubb = anno;
+        listaLibri[indice]._genere = genere;
+        listaLibri[indice]._prezzo = prezzo;
+    }
+    
     localStorage.setItem('libri', JSON.stringify(listaLibri));
     let rigaLibro = document.querySelector('.libro');
     rigaLibro.innerHTML = '';
@@ -94,17 +107,44 @@ function svuotaCampi() {
 
 function caricaLibri() {
     let rigaLibro = document.querySelector('.libro');
+    rigaLibro.innerHTML = '';
     console.log(listaLibri);
-    listaLibri.forEach((singleLibro) => {
-        creaRiga(singleLibro, rigaLibro);
+    listaLibri.forEach((singleLibro, index) => {
+        creaRiga(singleLibro, rigaLibro, index);
     })
 }
-function creaRiga(singleLibro, rigaLibro) {
+function creaRiga(singleLibro, rigaLibro, index) {
+    console.log(index);
     let tR = document.createElement('tr');
     tR.innerHTML = `<td> ${singleLibro.titolo} </td> 
                         <td> ${singleLibro.autore} </td> 
                         <td> ${singleLibro._annoPubb} </td> 
                         <td> ${singleLibro._genere} </td> 
-                        <td> ${singleLibro._prezzo} </td> `;
+                        <td> ${singleLibro._prezzo} </td> 
+                        <td><button class="btn btn-warning" onClick="modifica(${index})">Modifica</button></td>
+                        <td><button class="btn btn-danger" onClick="elimina(${index})">Elimina</button></td>`
+
     rigaLibro.appendChild(tR);
+}
+
+function modifica(index) {
+    console.log(index);
+    document.querySelector('#indice').value = index;
+    document.querySelector('#titolo').value = listaLibri[index].titolo;
+    document.querySelector('#autore ').value = listaLibri[index].autore;
+    document.querySelector('#anno').value = listaLibri[index]._annoPubb;
+    document.querySelector('#genere').value = listaLibri[index]._genere;
+    document.querySelector('#prezzo').value = listaLibri[index]._prezzo;
+
+}
+
+// .splice() per eliminare un elemento dall'array
+function elimina(index) {
+    //alert(index)
+    //1. elimino l'elemento dall'array
+    listaLibri.splice(index, 1);
+    //2. salvo nel local storage
+    localStorage.setItem('libri', JSON.stringify(listaLibri));
+    //3. ristampo la lista
+    caricaLibri();
 }
